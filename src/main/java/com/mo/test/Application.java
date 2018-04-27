aa716104330e4c780ff9edc945f5de31b2e266ef
moxingwang <chris.mo@chinaredstar.com>
2018-04-27 13:17:08 +0800

    commit

--- src/main/java/com/mo/test/Application.java
+++ src/main/java/com/mo/test/Application.java
@@ -1,14 +1,13 @@
 package com.mo.test;
 
 import org.apache.log4j.Logger;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.context.annotation.ImportResource;
 
-@EnableAutoConfiguration
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceTransactionManagerAutoConfiguration.class, DataSourceAutoConfiguration.class})
 @ImportResource({"classpath:spring/*.xml"})
 public class Application {
     private static Logger logger = Logger.getLogger(Application.class);
