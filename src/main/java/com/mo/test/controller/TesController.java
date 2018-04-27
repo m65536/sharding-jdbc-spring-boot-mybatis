98fe6bfe4fe02df44701b1d9aaaf6b966400c746
moxingwang <chris.mo@chinaredstar.com>
2018-04-27 14:59:11 +0800

    commit

--- src/main/java/com/mo/test/controller/TesController.java
+++ src/main/java/com/mo/test/controller/TesController.java
@@ -1,5 +1,7 @@
 package com.mo.test.controller;
 
+import com.mo.test.mapper.OrderItemMapper;
+import com.mo.test.mapper.OrderMapper;
 import com.mo.test.service.impl.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,14 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by MoXingwang on 2017/6/14.
  */
 @RestController
-@RequestMapping(value = "test")
+@RequestMapping(value = "")
 public class TesController {
 
     @Autowired
     private OrderService orderService;
+    @Autowired
+    private OrderMapper orderMapper;
+    @Autowired
+    private OrderItemMapper orderItemMapper;
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String test(){
+    @RequestMapping(value = "/init", method = RequestMethod.GET)
+    public String inittable() {
+        orderMapper.createIfNotExistsTable();
+        orderItemMapper.createIfNotExistsTable();
 
         return "";
     }
