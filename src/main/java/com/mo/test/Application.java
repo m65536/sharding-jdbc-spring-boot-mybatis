430e531d76bf22d152dd91c9b14aca47d978787c
moxingwang <chris.mo@chinaredstar.com>
2018-04-27 11:10:59 +0800

    commit

--- src/main/java/com/mo/test/Application.java
+++ src/main/java/com/mo/test/Application.java
@@ -2,15 +2,11 @@ package com.mo.test;
 
 import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
-@EnableAutoConfiguration
 @SpringBootApplication
-@ImportResource(locations = {
-        "classpath*:spring/*.xml",
-})
+@ImportResource({"classpath:spring/*.xml"})
 public class Application {
     private static Logger logger = Logger.getLogger(Application.class);
 
@@ -21,8 +17,6 @@ public class Application {
         SpringApplication.run(Application.class, args);
         logger.info("SpringBoot Start Success");
     }
-
-
 
 
 }
