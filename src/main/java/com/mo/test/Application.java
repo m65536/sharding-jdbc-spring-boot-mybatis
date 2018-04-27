01bc60b1078e3db8fb73dfc10ab8825d552ca26d
moxingwang <chris.mo@chinaredstar.com>
2018-04-27 13:17:55 +0800

    commit 启动成功

--- src/main/java/com/mo/test/Application.java
+++ src/main/java/com/mo/test/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.context.annotation.ImportResource;
 
+//exclude = {DataSourceTransactionManagerAutoConfiguration.class, DataSourceAutoConfiguration.class} 这个顺序不能颠倒
 @SpringBootApplication(exclude = {DataSourceTransactionManagerAutoConfiguration.class, DataSourceAutoConfiguration.class})
 @ImportResource({"classpath:spring/*.xml"})
 public class Application {
