a1687ad3be60fda8d26279bac7f73eca8f6fa600
xingwang.mo <xingwang.mo@chinaredstar.com>
2018-04-29 22:27:25 +0800

    commit

---
+++ spring-boot/src/main/java/com/mo/test/annotation/ShardingMasterHint.java
@@ -0,0 +1,17 @@
+package com.mo.test.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author MoXingwang on 2018/4/29.
+ * 强制路由主库
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ShardingMasterHint {
+    String note() default "";
+
+}
---
+++ spring-boot/src/main/java/com/mo/test/annotation/ShardingMasterHintAop.java
@@ -0,0 +1,37 @@
+package com.mo.test.annotation;
+
+import io.shardingjdbc.core.api.HintManager;
+import io.shardingjdbc.core.hint.HintManagerHolder;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author MoXingwang on 2018/4/29.
+ */
+@Aspect
+@Component
+@Order(value = Integer.MAX_VALUE)
+public class ShardingMasterHintAop {
+    @Pointcut("@annotation(ShardingMasterHint)")
+    public void hint(){
+
+    }
+
+    @Before("hint()")
+    public void doHint(JoinPoint Point){
+        HintManager hintManager = HintManager.getInstance();
+        hintManager.setMasterRouteOnly();
+    }
+
+    @After("hint()")
+    public void clear(JoinPoint Point) {
+        HintManagerHolder.get().close();
+    }
+
+
+}
