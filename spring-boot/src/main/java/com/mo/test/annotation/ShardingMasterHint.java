16de4728cb5ed8561bfcf9b71eecbaf2a1953dfa
xingwang.mo <xingwang.mo@chinaredstar.com>
2018-04-29 22:30:44 +0800

    commit

--- spring-boot/src/main/java/com/mo/test/annotation/ShardingMasterHint.java
+++ spring-boot/src/main/java/com/mo/test/annotation/ShardingMasterHint.java
@@ -12,6 +12,6 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ShardingMasterHint {
-    String note() default "";
+    String hint() default "";
 
 }
