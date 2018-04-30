8b6fe5ff71b1301f1bd16a53ee10e62d65cea38d
moxingwang <chris.mo@chinaredstar.com>
2018-04-30 16:46:25 +0800

    jar 冲突导致aop报错

--- jdbc-mapper/src/main/java/com/mo/test/sharding/DatabaseComplexKeysShardingAlgorithm.java
+++ jdbc-mapper/src/main/java/com/mo/test/sharding/DatabaseComplexKeysShardingAlgorithm.java
@@ -1,9 +1,12 @@
 package com.mo.test.sharding;
 
+import io.shardingjdbc.core.api.algorithm.sharding.ListShardingValue;
 import io.shardingjdbc.core.api.algorithm.sharding.ShardingValue;
 import io.shardingjdbc.core.api.algorithm.sharding.complex.ComplexKeysShardingAlgorithm;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 /**
  * @author MoXingwang on 2018/4/30.
@@ -12,5 +15,21 @@ public class DatabaseComplexKeysShardingAlgorithm implements ComplexKeysSharding
     @Override
     public Collection<String> doSharding(Collection<String> collection, Collection<ShardingValue> collection1) {
         return null;
+    }
+
+
+    private Collection<Long> getShardingValue(Collection<ShardingValue> shardingValues, final String key) {
+        Collection<Long> valueSet = new ArrayList<>();
+        Iterator<ShardingValue> iterator = shardingValues.iterator();
+        while (iterator.hasNext()) {
+            ShardingValue next = iterator.next();
+            if (next instanceof ListShardingValue) {
+                ListShardingValue value = (ListShardingValue) next;
+                if (value.getColumnName().equals(key)) {
+                    return value.getValues();
+                }
+            }
+        }
+        return valueSet;
     }
 }
