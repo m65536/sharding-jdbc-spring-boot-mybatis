c0f854a1a7042537c7c20a323800b59d3c186e36
moxingwang <chris.mo@chinaredstar.com>
2018-04-30 13:35:10 +0800

    jar 冲突导致aop报错

---
+++ jdbc-mapper/src/main/java/com/mo/test/sharding/TxOrderComplexKeysShardingAlgorithm.java
@@ -0,0 +1,16 @@
+package com.mo.test.sharding;
+
+import io.shardingjdbc.core.api.algorithm.sharding.ShardingValue;
+import io.shardingjdbc.core.api.algorithm.sharding.complex.ComplexKeysShardingAlgorithm;
+
+import java.util.Collection;
+
+/**
+ * @author MoXingwang on 2018/4/30.
+ */
+public class TxOrderComplexKeysShardingAlgorithm implements ComplexKeysShardingAlgorithm {
+    @Override
+    public Collection<String> doSharding(Collection<String> collection, Collection<ShardingValue> collection1) {
+        return null;
+    }
+}
