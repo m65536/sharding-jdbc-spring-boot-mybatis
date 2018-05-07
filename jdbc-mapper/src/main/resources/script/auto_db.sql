95a28228b3b4ef81e324840ffeb3c53db418e4d5
moxingwang <chris.mo@chinaredstar.com>
2018-05-07 17:24:36 +0800

    commit

--- jdbc-mapper/src/main/resources/script/auto_db.sql
+++ jdbc-mapper/src/main/resources/script/auto_db.sql
@@ -1,9 +1,5 @@
-DROP SCHEMA IF EXISTS tx_order_master_0;
-DROP SCHEMA IF EXISTS tx_order_master_1;
-DROP SCHEMA IF EXISTS tx_order_slave_0;
-DROP SCHEMA IF EXISTS tx_order_slave_1;
+DROP SCHEMA IF EXISTS tx_order_0;
+DROP SCHEMA IF EXISTS tx_order_1;
 
-CREATE SCHEMA IF NOT EXISTS tx_order_master_0;
-CREATE SCHEMA IF NOT EXISTS tx_order_master_1;
-CREATE SCHEMA IF NOT EXISTS tx_order_slave_0;
-CREATE SCHEMA IF NOT EXISTS tx_order_slave_1;
+CREATE SCHEMA IF NOT EXISTS tx_order_0;
+CREATE SCHEMA IF NOT EXISTS tx_order_1;
